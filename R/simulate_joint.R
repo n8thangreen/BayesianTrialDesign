f8088aba5712153de176230d2af196efde56c014
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-03-29 11:27:34 +0100

    fixed code with new parameterisation still need to check that havent introduced a bug eg swapping control and treatment wrong way round!

--- R/simulate_joint.R
+++ R/simulate_joint.R
@@ -1,35 +1,36 @@
 
-#'
+#' @importFrom tibble lst
 #' @export
 normal_distn <- function(mu = 1, sigma = 1, .data = NULL) 
   .data$prior <- tibble::lst(mu, sigma)
 
-#'
+#' @importFrom tibble lst
 #' @export
 beta_distn <- function(a = 1, b = 1, .data = NULL) 
   .data$prior <- tibble::lst(a, b)
 
 
-#'
+#' @importFrom tibble lst
 #' @export
 simulate_joint <- function(input, no_of_sim = 1000) {
-  do.call(simulate_joint_, c(input, no_of_sim))
+  input <- modifyList(tibble::lst(no_of_sim), input)
+  do.call(simulate_joint_, input)
 }
 
 
 #' simulate_joint_
 #' 
-#' @param p_control 
-#' @param p_treatment 
-#' @param prob_accept_ha 
-#' @param total_sample_size 
-#' @param no_of_sim 
-#' @param alternative 
-#' @param prior_p_control 
-#' @param prior_log_odds_ratio 
-#' @param n_iter 
-#' @param n_burnin 
-#' @param n_thin
+#' @param p_control Control probability
+#' @param p_treatment  Treatment probability
+#' @param prob_accept_ha Probability accept alternative hypothesis
+#' @param total_sample_size Total sample size
+#' @param no_of_sim Number of simulations
+#' @param alternative Alternative test
+#' @param prior_p_control Prior for control probability
+#' @param prior_log_odds_ratio Prior for log-odds ratio
+#' @param n_iter Number of iterations
+#' @param n_burnin Number of burn-in
+#' @param n_thin Number of thinning
 #' 
 #' @examples
 #' 
@@ -63,24 +64,26 @@ simulate_joint_ <- function(p_control = 0.5,
   if (is.na(p_treatment))
     p_treatment <- boot::inv.logit(log(odds_ratio) + boot::logit(p_control))
   
-  # number of successes
-  z1 <- rbinom(n = no_of_sim, size = total_sample_size, prob = p_control)
-  z2 <- rbinom(n = no_of_sim, size = total_sample_size, prob = p_treatment)
+  rand_prop <- randomisation_ratio/(randomisation_ratio + 1)
+  n_control <- round(rand_prop*total_sample_size, 0)
+  n_treatment <- total_sample_size - n_control
   
+  # number of successes
+  z_control <- rbinom(n = no_of_sim, size = n_control, prob = p_control)
+  z_treatment <- rbinom(n = no_of_sim, size = n_treatment, prob = p_treatment)
+
   ##########
   # Stan
   
   rstan::rstan_options(auto_write = TRUE)
   options(mc.cores = parallel::detectCores())
   
-  rand_prop <- randomisation_ratio/(randomisation_ratio + 1)
-  
   dat_input <-
     list(
       nsim = no_of_sim,
       narms = 2,
-      N = c(rand_prop*total_sample_size, (1 - rand_prop)*total_sample_size),
-      z = cbind(z1, z2),
+      N = c(n_control, n_treatment),
+      z = cbind(z_control, z_treatment),
       a = prior_p_control$a,
       b = prior_p_control$b,
       mu = prior_log_odds_ratio$mu,
@@ -106,6 +109,7 @@ simulate_joint_ <- function(p_control = 0.5,
   
   save(stan_output, file = here::here("data", "stan_output_correlated.RData"))
   
+  browser()
   tibble::lst(
     prob_gt_0 = colMeans(alt*stan_output$thetaDiff > 0),
     positives = prob_gt_0 >= prob_accept_ha,        # record if trial is "positive"
